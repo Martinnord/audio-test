7ba97da424376e093c5b38c1ccf474e5166bece8
Martin Nordström <martin.nordstrom99@gmail.com>
2020-06-16 15:02:36 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component, createRef } from "react";
-import UIfx from "uifx";
-
 import logo from "./logo.svg";
 import Chime from "./chime.wav";
 import "./App.css";
-
-const sound = new UIfx(Chime, { volume: 0.9 });
 
 class App extends Component {
   constructor(props) {
@@ -16,37 +12,26 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    if (this.ref.current !== null) {
-      this.ref.current.click();
-      // this.ref.current.play();
-    }
-  }
+  // componentDidMount() {
+  //   if (this.ref.current !== null) {
+  //     this.ref.current.click();
+  //     // this.ref.current.play();
+  //   }
+  // }
 
-  handleClick = () => {
+  onPlay = () => {
+    this.setState({ muted: false });
     this.ref.current.play();
   };
 
-  toggleMuted = () => {
-    this.setState((curr) => ({ muted: !curr.muted }));
-  };
-
   render() {
-    console.log("sound", sound);
-
     console.log(this.ref.current);
 
     const { muted } = this.state;
 
     return (
-      <div className="App">
-        <audio
-          loop
-          ref={this.ref}
-          src={Chime}
-          onClick={() => console.log("CLICKED")}
-        />
-        <button onClick={() => sound.play(0.6)}>PLAY</button>
+      <div className="App" style={{ height: "100vh" }} onClick={this.onPlay}>
+        <audio loop autoPlay muted={muted} ref={this.ref} src={Chime} />
         <button onClick={this.toggleMuted}>
           MUTED: {muted ? "YES" : "NO"}
         </button>
